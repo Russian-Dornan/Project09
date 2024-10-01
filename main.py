de2cbc586715411dd73ad2ee92f14fe438ba0aed
Russian-Dornan <woddow1@gmail.com>
2024-10-01 15:21:50 +0300

    Draw func
    Создана функция отрисовки предметов

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
 import pygame
 import test
+import draw
 import parameters as par
 
 pygame.init() #Просто запуск pygame
 
 
-win = pygame.display.set_mode((wide, hight))    # Создание окна !win! 1200*700
+win = pygame.display.set_mode((par.wide, par.hight))    # Создание окна !win! 1200*700
 size_stand=24
 font = pygame.font.SysFont("", size_stand, 'utf-8')     #Создание Шрифта
 
@@ -18,16 +19,16 @@ fps=60
 
 speed=10000
 
-item={}# 1:[x, y, size, Speed_x, Speed_y, mass, Color, ]
+item={1: [100, 100, 20, 0, 0, 10, par.RED]}# 1:[x, y, size, Speed_x, Speed_y, mass, Color, ]
 
 while Work:
+    cloak.tick(fps * speed);  # Контроль фпс
+    win.fill(background_color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:   #Выход из программы
             Work = False
 
-    cloak.tick(fps*speed);  #Контроль фпс
-
-    win.fill(background_color)
-    #pygame.draw.rect(win, (255, 255, 255), (x, y, 20, 20))#Создание квадратика 20 на 20
+    for i in item:
+        draw.DrawItem(item[i], win)
 
     pygame.display.update();    #Обновление экрана
