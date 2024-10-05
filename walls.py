acc69b2d59c78d97c2b60af86160d47e88c19edb
LABARAT <ligma.00@mail.ru>
2024-10-05 18:48:51 +0300

    сделал стены

--- walls.py
+++ walls.py
@@ -1,4 +1,5 @@
 #Суорость должна меняться на противоположную, в стенках не должен объект застревать
-def Reverse(speed, pos):
-
-    return speed, pos
+def Reverse(speed, pos_x, pos_y):
+    if (pos_x == 0) or (pos_y == 0) or (pos_x == 1200) or (pos_y == 700):
+        speed == (-1) * speed
+    return speed, pos_x, pos_y
