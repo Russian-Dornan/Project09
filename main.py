487df48008f3f33734fc88a3ed3b07b33f277ea0
RomanKob <roma.kobelckov@yandex.ru>
2024-10-08 21:58:38 +0300

    Создание кнопок
    Добавил в draw.py метод GetButton для создания кнопок, ChangeColor для того, чтобы кнопки меняли цвет при наведении. Добавил в main.py обработчик событий нажатий кнопок

--- main.py
+++ main.py
@@ -25,11 +25,23 @@ item={1: [100, 100, 20, 0.2, 0.1, 10, par.RED]}# 1:[x, y, size, Speed_x, Speed_y
 while Work:
     cloak.tick(fps * speed);  # Контроль фпс
     win.fill(background_color)
+    buttons =[
+    draw.GetButton(20, 10, 100, 40,'Добавить объект', win), #Массив с кнопками
+    draw.GetButton(20, 60, 90, 40,'Выйти', win)
+    ]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:   #Выход из программы
+            Work = False
+    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  #Обработчик событий для кнопок
+        if buttons[0].collidepoint(event.pos):
+            print('Ура!')
+            pygame.time.delay(200)
+        if buttons[1].collidepoint(event.pos):
             Work = False
 
     for i in item:
         draw.DrawItem(item[i], win)
         proc.Item_Update(item, i)
     pygame.display.update();    #Обновление экрана
+
+pygame.quit()
