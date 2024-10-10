5d3ae08b035d225a25a82b5209a367f084aa9eec
Russian-Dornan <woddow1@gmail.com>
2024-10-10 21:06:09 +0300

    Переходы
    Создал переход из состояний

--- main.py
+++ main.py
@@ -21,23 +21,32 @@ fps=60
 speed=10000
 
 item={1: [100, 100, 20, 0.2, 0.1, 10, par.RED]}# 1:[x, y, size, Speed_x, Speed_y, mass, Color, ]
-
+menu=1
 while Work:
     cloak.tick(fps * speed);  # Контроль фпс
     win.fill(background_color)
-    buttons =[
-    draw.GetButton(20, 10, 100, 40,'Добавить объект', win), #Массив с кнопками
-    draw.GetButton(20, 60, 90, 40,'Выйти', win)
-    ]
+    if(menu==1):
+        buttons =[
+        draw.GetButton(20, 10, 100, 40,'Добавить объект', win), #Массив с кнопками
+        draw.GetButton(20, 60, 90, 40,'Выйти', win)
+        ]
+    elif(menu==2):
+        buttons = [
+
+            draw.GetButton(20, 60, 90, 40, 'Выйти', win)
+        ]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:   #Выход из программы
             Work = False
     if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  #Обработчик событий для кнопок
-        if buttons[0].collidepoint(event.pos):
-            print('Ура!')
+        if buttons[0].collidepoint(event.pos) and menu==1:
+            menu=2
             pygame.time.delay(200)
-        if buttons[1].collidepoint(event.pos):
-            Work = False
+        elif buttons[0].collidepoint(event.pos) and menu==2:
+            menu=1
+            pygame.time.delay(200)
+        elif buttons[1].collidepoint(event.pos):
+            Work=False
 
     for i in item:
         draw.DrawItem(item[i], win)
