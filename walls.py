393f94b6640af3a2e5a22e1183ac2fde00209ac7
UhUh123 <ligma.00@mail.ru>
2024-10-19 18:36:43 +0300

    доделал стенки.py

--- walls.py
+++ walls.py
@@ -2,7 +2,7 @@
 from parameters import *
 def Reverse(x,y,speed_x,speed_y):
     if (x == 0) or (x == (wide-otstup)):
-        speed_x == (-1) * speed_x
-    elif (y == 0) or (y == hight):
-        speed_y == (-1) * speed_y
+        speed_x = (-1) * speed_x
+    elif (y == 0) or (y == (hight-otstup)):
+        speed_y = (-1) * speed_y
     return x,y,speed_x,speed_y
