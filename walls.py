38976a4344620f44338c72f1674e716e5cbe8436
UhUh123 <ligma.00@mail.ru>
2024-10-19 18:39:29 +0300

    Доделал 3 walls.py

--- walls.py
+++ walls.py
@@ -3,6 +3,6 @@ from parameters import *
 def Reverse(x,y,speed_x,speed_y):
     if (x == 0) or (x == (wide-massive[3])):
         speed_x = (-1) * speed_x
-    elif (y == 0) or (y == (hight-massive[4])):
+    elif (y == 0) or (y == (hight-massive[3])):
         speed_y = (-1) * speed_y
     return x,y,speed_x,speed_y
