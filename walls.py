fb88ccd7726d9fc7940d09e949e9e1be385f57b6
UhUh123 <ligma.00@mail.ru>
2024-10-19 19:09:51 +0300

    улучшил 6 walls.py

--- walls.py
+++ walls.py
@@ -1,9 +1,9 @@
 #Суорость должна меняться на противоположную, в стенках не должен объект застревать
 from parameters import *
 import processor as proc
-def Reverse(x,y,speed_x,speed_y):
-    if (x == 0) or (x == (wide-massive[3]-otstup)):
+def Reverse(x,y,size,speed_x,speed_y):
+    if (x == 0) or (x == (wide-size-otstup)):
         speed_x = (-1) * speed_x
-    elif (y == 0) or (y == (hight-massive[3])):
+    elif (y == 0) or (y == (hight-size)):
         speed_y = (-1) * speed_y
     return x,y,speed_x,speed_y
