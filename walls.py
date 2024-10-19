c99c9ea2889fbf084356c2780fd37205102c3e2a
UhUh123 <ligma.00@mail.ru>
2024-10-19 19:14:17 +0300

    Update 7 walls.py

--- walls.py
+++ walls.py
@@ -2,8 +2,8 @@
 from parameters import *
 import processor as proc
 def Reverse(x,y,size,speed_x,speed_y):
-    if (x == 0) or (x == (wide-size-otstup)):
+    if (x <= 0) or (x >= (wide-size-otstup)):
         speed_x = (-1) * speed_x
-    elif (y == 0) or (y == (hight-size)):
+    elif (y <= 0) or (y >= (hight-size)):
         speed_y = (-1) * speed_y
     return x,y,speed_x,speed_y
